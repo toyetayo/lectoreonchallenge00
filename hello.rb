c2de9436ae5d29bfe6549b0c07579137b41072fb
Timothy Oyetayo <toyetayo@academic.rrc.ca>
2024-05-08 07:00:58 -0500

    updated hello.rb

--- hello.rb
+++ hello.rb
@@ -1,1 +1,15 @@
 puts "Hello World!"
+
+
+def say_goodnight(name)
+    puts "Goodnight, #{name}."
+end
+
+say_goodnight"Timothy"
+
+4.times { puts 'Curly braces!' }
+
+4.times do
+    puts 'Do end blocks'
+end
+
